1aa11d9803474d6f7df54aa06c7b722c745813f8
Boris <70476304+Kujojotar@users.noreply.github.com>
2022-09-26 19:13:29 +0800

    Update ChatApplication.java

--- src/main/java/com/james/chat/ChatApplication.java
+++ src/main/java/com/james/chat/ChatApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/**
- * @author 聂戬
- * 银行业务系统的启动类
- */
 @SpringBootApplication
 @EnableCaching
 @EnableTransactionManagement
